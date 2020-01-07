222d730389170dbc84f53dd5646a223176ded2e1
EverythingYu <asdfjoeasdfjoe@gmail.com>
2020-01-07 21:59:40 +0800

    remove old file

--- deep_example.py
+++ deep_example.py
@@ -14,12 +14,8 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.read_csv("Data Set update2.csv",encoding="utf-8")
-
-
+df = pd.read_csv("Data.csv",encoding="utf-8")
 model = models.Sequential()
-
-
 		     #過濾器數量 ↓      ↓過濾器長寬
 model.add(layers.Conv1D(256, kernel_size=1, strides=1,activation='relu', input_shape=(1
  , 4))) # 加入 Covn1d 層
@@ -27,9 +23,6 @@ model.add(layers.MaxPooling1D(1, strides=1)) # 進行 MaxPooling
 model.add(layers.Conv1D(128, (1), activation='relu'))
 model.add(layers.MaxPooling1D(1, strides=1))
 model.add(layers.Conv1D(64, (1), activation='relu'))
-
-
-
 model.summary()
 
 
@@ -48,8 +41,6 @@ model.summary()
 # In[3]:
 
 
-from keras.datasets import mnist
-from keras.utils import to_categorical 
 from sklearn.model_selection import train_test_split 
 
 df = np.array(df)
@@ -57,7 +48,7 @@ np.random.shuffle(df)
 Dependent = df[:, 4:8] #y
 Independent = df[:, 0:4] # the rest of the columns (commonly refered to as "X"
 
-X_train, x_test, y_train, y_test = train_test_split(Independent, Dependent,test_size=0.4,random_state=42)
+X_train, x_test, y_train, y_test = train_test_split(Independent, Dependent,test_size=0.4,random_state=0)
 
 print(X_train.shape) 
 print(x_test.shape) 
@@ -67,12 +58,12 @@ print("--------------------------------------")
 X_train = X_train[:,np.newaxis]
 X_train = X_train.astype('float32')/100
 
-x_test = x_test[:,np.newaxis]
- 
+x_test = x_test[:,np.newaxis] 
 x_test = x_test.astype('float32')/100
-y_train = y_train[:,np.newaxis]
 
+y_train = y_train[:,np.newaxis]
 y_train = y_train.astype('float32')/100
+
 y_test = y_test[:,np.newaxis]
 y_test = y_test.astype('float32')/100
 
@@ -84,5 +75,5 @@ print(y_test.shape)
 model.compile(optimizer='Adam',
 loss='categorical_crossentropy',
 metrics=['accuracy'])
-model.fit(X_train, y_train,validation_data=(x_test,y_test) ,epochs=5, batch_size=5)
+model.fit(X_train, y_train,validation_data=(x_test,y_test) ,epochs=5, batch_size=4)
 
